29e5afef1cc139d457b5f4543166e005ee73dc8a
Abenezer Golda <abenygolda@gmail.com>
2022-03-10 13:51:25 -0800

    C-Hello, World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,3 +1,8 @@
+/*
+ * File: 101-quote.c
+ * Auth: Brennan D Baraban
+ */
+
 #include <unistd.h>
 
 /**
@@ -8,9 +13,10 @@
  */
 int main(void)
 {
-        write(2,
-              "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-              59);
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
 
-        return (1);
+	return (1);
 }
+
