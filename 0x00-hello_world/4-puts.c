f59e95d33484e5b37e4e395b040848d105499f96
Abenezer Golda <abenygolda@gmail.com>
2022-03-10 11:09:35 -0800

    C-Hello, World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 #include<string.h>
 int main(void)
 {
-char str[] = "Programming is like building a multilingual puzzle";
+char str[] = "\"Programming is like building a multilingual puzzle";
 
 puts(str);
 
