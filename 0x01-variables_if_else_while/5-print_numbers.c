7c0265d16f10b707a223628d023a227f2c57a695
Abenezer Golda <abenygolda@gmail.com>
2022-03-13 01:37:27 -0800

    variable, conditional, and loops

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,6 +1,6 @@
 /*
  * File: 5-print_numbers.c
- * Auth: Brennan D Baraban
+ * Auth: Abenezer Golda
  */
 
 #include <stdio.h>
--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,6 +1,6 @@
 /*
  * File: 6-print_numberz.c
- * Auth: Brennan D Baraban
+ * Auth: Abenezer Golda
  */
 
 #include <stdio.h>
