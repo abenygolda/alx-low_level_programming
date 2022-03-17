ba254a61f63ed5408243cd69133bc0ae2db244e5
Abenezer Golda <abenygolda@gmail.com>
2022-03-17 11:37:06 -0700

    more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "math.h"
+/**
+ * main - checks whether the input is upper case or lower case.
+ *
+ * It returns one if it's upper case and returns 0 if it's lower case character. 
+ * Author: ABENEZER GOLDA
+ */
+int _isupper(int c){
+char c;
+if (c > 'A' && c < 'Z')
+	return (1);
+else 	
+	return (0);
+}
