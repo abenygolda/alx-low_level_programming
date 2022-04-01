2b1fc0c0a4941e4867ed7242957472de70ac4c0e
Abenezer Golda <abenygolda@gmail.com>
2022-04-01 07:00:51 -0700

    C-argc, argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,12 @@
+/*this is about the argc ana argv
+ *I am abenezer Golda 
+*/
+
 #include<stdio.h>
 #include<stdlib.h>
 
-int main (int argc, char* argv[]){
+int main (int argc, char* argv[])//this is a main function that it takes arguments.
+{
 int i;
 for(i=0;i<argc;i++)
 	printf("argv[%d] = %s\n", i, argv[i]);
